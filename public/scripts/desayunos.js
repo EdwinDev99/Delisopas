e2feb7864ad229cff0a49825c33aebf670f4a9a1
EdwinDev99 <edwindev99@gmail.com>
2025-03-13 18:11:29 +0000

    agregar la logica para sumar los desayunos

--- public/scripts/desayunos.js
+++ public/scripts/desayunos.js
@@ -1,3 +1,5 @@
+let precioTotal = 0;
+
 let pedidoActual = { mesa: "", productos: [], total: 0 };
 
 const closeModalDesayunos = document.getElementById("close-icon2");
@@ -9,6 +11,7 @@ closeModalDesayunos.addEventListener("click", () => {
 function agregarDesayuno(nombre, precio) {
   pedidoActual.productos.push({ nombre, precio });
   pedidoActual.total += precio;
+  console.log("el total es", pedidoActual.total);
 
   const modal = document.getElementById("modalDesayunos");
   if (modal) {
@@ -17,3 +20,60 @@ function agregarDesayuno(nombre, precio) {
     console.error("No se encontró el modal en el DOM");
   }
 }
+
+document
+  .getElementById("enviar-desatuno")
+  .addEventListener("click", function () {
+    const inputs = document.querySelectorAll("input[type='number']"); // Selecciona solo los inputs numéricos
+    inputs.forEach((input) => {
+      const cantidad = parseInt(input.value) || 0; // Convierte a número y evita NaN
+
+      switch (input.id) {
+        case "changua":
+          precioTotal += 6500 * cantidad;
+          break;
+        case "caldo":
+          precioTotal += 7500 * cantidad;
+          break;
+        case "chocoPan":
+          precioTotal += 3500 * cantidad;
+          break;
+        case "huevosArroz":
+          precioTotal += 4500 * cantidad;
+          break;
+        case "huevoGusto":
+          precioTotal += 3500 * cantidad;
+          break;
+        case "TamalEspecial":
+          precioTotal += 8000 * cantidad;
+          break;
+        case "cafe":
+          precioTotal += 3000 * cantidad;
+          break;
+        case "chocolate":
+          precioTotal += 3000 * cantidad;
+          break;
+        case "perico":
+          precioTotal += 2000 * cantidad;
+          break;
+        case "jugoHit":
+          precioTotal += 2000 * cantidad;
+          break;
+        case "jugoHitG":
+          precioTotal += 3000 * cantidad;
+          break;
+        case "jugoNatural":
+          precioTotal += 4500 * cantidad;
+          break;
+        case "JNaruralLeche":
+          precioTotal += 5500 * cantidad;
+          break;
+        default:
+          console.warn(
+            `El producto con ID "${input.id}" no tiene un precio asignado.`
+          );
+      }
+    });
+
+    console.log(`Total a pagar: ${precioTotal} COP`);
+  });
